beb8e873b7e26189471a50e365dae5b1950baba1
arjunTltech <arjunntltechnologies@gmail.com>
2025-05-14 19:17:03 +0530

    feat: added web organizatioin

--- src/route/web/indexRoute.js
+++ src/route/web/indexRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import socialRoutes from "./socialRoute.js";
+import organizationDetailsRoutes from "./organizationDetailsRoute.js";
 
 
 
 const router = express.Router();
 router.use('/social', socialRoutes);
+router.use('/organization', organizationDetailsRoutes)
 
 
 
