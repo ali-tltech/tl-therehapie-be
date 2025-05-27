1e3f265eb5e663654c4dbf56ac011f4c50480c94
arjunTltech <arjunntltechnologies@gmail.com>
2025-05-27 15:36:51 +0530

    feat: added testimonial module

--- src/route/web/indexRoute.js
+++ src/route/web/indexRoute.js
@@ -2,10 +2,11 @@ import express from "express";
 
 import socialRoutes from "./socialRoute.js";
 import organizationDetailsRoutes from "./organizationDetailsRoute.js";
-import enquiriesRoutes from "./enquiries.routes.js";
+import enquiriesRoutes from "./enquiriesRoute.js";
 import faqRoutes from "./faqRoute.js";
-import newsletterRoutes from "./newsletter.routes.js";
-import blogRoutes from "./blog.routes.js";
+import newsletterRoutes from "./newsletterRoute.js";
+import blogRoutes from "./blogRoute.js";
+import testimonialRoutes from "./testimonialRoute.js";
 
 
 
@@ -16,6 +17,7 @@ router.use("/enquiries", enquiriesRoutes);
 router.use("/faqs", faqRoutes);
 router.use("/newsletter", newsletterRoutes);
 router.use("/blog",blogRoutes);
+router.use("/testimonial", testimonialRoutes);
 
 
 
